84e973e240cafbfca6f731a4fc8051bf8a8c1ed9
chenzhijie <chenzhijie@shuwen.com>
2022-05-03 14:35:29 +0800

    feat:路由拦截

---
+++ micro/router/routerHandler.js
@@ -0,0 +1,10 @@
+// import { lifecycle } from "../lifecycle/lifecycle"
+// import { isTurnChild } from "../util"
+export const turnApp = async () => {
+  console.log('路由切换');
+  // // 查看当前路由是否有变化
+  // if (isTurnChild()) {
+  //   // 路由变化，同步修改子应用
+  //   await lifecycle()
+  // }
+}
--- micro/start.js
+++ micro/start.js
@@ -1,4 +1,9 @@
 import { setList } from "./constants/subApps"
+import { rewriteRouter } from "./router/rewriteRouter"
+
+// 实现路由拦截
+rewriteRouter()
+
 // 注册子应用列表
 export const registerMicroApps = (appList) => {
   setList(appList)
